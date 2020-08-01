0efb4ae08fd1faa64c2faf66c298a587da0c53e9
龙少5520 <longshao5520@outlook.com>
2020-08-01 15:10:05 +0800

    解决标头问题&加载动画

---
+++ admin/vue.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-01 14:42:51
+ * @LastEditTime: 2020-08-01 14:42:58
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Nest-Vue-Blog\admin\vue.config.js
+ */ 
+module.exports = {
+  devServer: {
+    disableHostCheck: true
+  }
+}
